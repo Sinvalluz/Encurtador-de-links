7dfc3b1657cad4d7d6eafa6aa3c4f8f0dc3a7323
Sinval Luz <contato.sinvalluz@gmail.com>
2025-10-07 08:53:06 -0300

    chore: folder structure

---
+++ src/main/java/br/sinvalDev/encurtadorDeLinks/EncurtadorDeLinksApplication.java
@@ -0,0 +1,13 @@
+package br.sinvalDev.encurtadorDeLinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorDeLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorDeLinksApplication.class, args);
+	}
+
+}
